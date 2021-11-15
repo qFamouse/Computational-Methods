3aa20404add2807590dac6b8b9fa0864053c218a
Famouse <famouse_ru@mail.ru>
2021-11-16 00:39:43 +0300

    Added two Euler methods to get the answer to the Cauchy problem

---
+++ Integrate/OdeSolvers/LeonhardEuler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ComputationalMethods.Integrate.OdeSolvers
+{
+    class LeonhardEuler
+    {
+        public static double[] Simple(double y0, double start, double end, int N, Func<double, double, double> f)
+        {
+            double[] y = new double[N];
+            y[0] = y0;
+            double h = (end - start) / (N-1); // N-1 -> Because there are actually partitions N-1 partitions
+            double x = start;
+
+            for (int i = 1; i < N; i++, x+=h)
+            {
+                y[i] = y[i - 1] + h * f(x, y[i - 1]);
+            }
+
+            return y;
+        }
+
+        public static double[] Improved(double y0, double start, double end, int N, Func<double, double, double> f)
+        {
+            double[] y = new double[N];
+            y[0] = y0;
+            double h = (end - start) / (N - 1); // N-1 -> Because there are actually partitions N-1 partitions
+            double h2 = h / 2; // Half of h
+            double x = start;
+
+            for (int i = 1; i < N; i++, x += h)
+            {
+                y[i] = y[i - 1] + h * f(x + h2, y[i-1] + h2 * f(x, y[i-1]));
+            }
+
+            return y;
+        }
+    }
+}
