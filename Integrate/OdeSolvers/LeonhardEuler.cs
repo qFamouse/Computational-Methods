985b8546b428e478892356205df1888f88f05d30
Famouse <famouse_ru@mail.ru>
2021-11-19 17:38:43 +0300

    Adding Euler-Cauchy Method

--- Integrate/OdeSolvers/LeonhardEuler.cs
+++ Integrate/OdeSolvers/LeonhardEuler.cs
@@ -7,11 +7,13 @@ namespace ComputationalMethods.Integrate.OdeSolvers
 {
     class LeonhardEuler
     {
+        #region Public Static Methods
+
         public static double[] Simple(double y0, double start, double end, int N, Func<double, double, double> f)
         {
             double[] y = new double[N];
             y[0] = y0;
-            double h = (end - start) / (N-1); // N-1 -> Because there are actually partitions N-1 partitions
+            double h = CalculateStep(start, end, N);
             double x = start;
 
             for (int i = 1; i < N; i++, x+=h)
@@ -26,7 +28,7 @@ namespace ComputationalMethods.Integrate.OdeSolvers
         {
             double[] y = new double[N];
             y[0] = y0;
-            double h = (end - start) / (N - 1); // N-1 -> Because there are actually partitions N-1 partitions
+            double h = CalculateStep(start, end, N);
             double h2 = h / 2; // Half of h
             double x = start;
 
@@ -37,5 +39,32 @@ namespace ComputationalMethods.Integrate.OdeSolvers
 
             return y;
         }
+
+        public static double[] CauchyImproved(double y0, double start, double end, int N, Func<double, double, double> f)
+        {
+            double[] y = new double[N];
+            y[0] = y0;
+            double h = CalculateStep(start, end, N);
+            double h2 = h / 2; // Half of h
+            double x = start;
+
+            for (int i = 1; i < N; i++, x += h)
+            {
+                y[i] = y[i - 1] + h2 * (f(x, y[i - 1]) + f(x + h, y[i - 1] + h * f(x, y[i - 1])));
+            }
+
+            return y;
+        }
+
+        #endregion
+
+        #region Private Static Methods
+
+        private static double CalculateStep(double start, double end, int N) // h
+        {
+            return (end - start) / (N - 1); // N-1 -> Because there are actually partitions N-1 partitions
+        }
+
+        #endregion
     }
 }
