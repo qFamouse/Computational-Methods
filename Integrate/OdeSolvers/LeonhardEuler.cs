cac9745157d5cdbf40933bb040b6bc46d10901b3
Famouse <famouse_ru@mail.ru>
2021-11-19 23:32:37 +0300

    LeonhardEuler to solve ODE SYSTEM

--- Integrate/OdeSolvers/LeonhardEuler.cs
+++ Integrate/OdeSolvers/LeonhardEuler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
@@ -8,6 +9,8 @@ namespace ComputationalMethods.Integrate.OdeSolvers
     class LeonhardEuler
     {
         #region Public Static Methods
+
+        #region Simple Method
 
         public static double[] Simple(double y0, double start, double end, int N, Func<double, double, double> f)
         {
@@ -23,6 +26,36 @@ namespace ComputationalMethods.Integrate.OdeSolvers
 
             return y;
         }
+
+        public static double[,] Simple(double[] y0, double start, double end, int N, Func<double, double[], double[]> f)
+        {
+            // Initial Values //
+            double[,] y = new double[N, y0.Length +1]; // +1 for 'x' values
+            double h = CalculateStep(start, end, N);
+            double x = start;
+            y[0, 0] = x;
+
+            for (int i = 0; i < y0.Length; i++)
+            {
+                y[0, i + 1] = y0[i]; // + 1 because on y[i,0] we have 'x' value
+            }
+            // Solution algorithm //
+            var yI = (double[])y0.Clone(); // Clone y0 because we need to edit it
+            for (int i = 1; i < N; i++) // row
+            {
+                double[] fI = f(x, yI); // Calculate functions value
+                y[i, 0] = x += h; // Put 'x+=h' value to array with go to next step
+                for (int j = 1; j <= y0.Length; j++) // column
+                {
+                    // Calculate to array and yI(for next calculating).
+                    yI[j - 1] = y[i, j] = y[i - 1, j] + h * fI[j - 1];
+                }
+            }
+            // Return //
+            return y;
+        }
+
+        #endregion
 
         public static double[] Improved(double y0, double start, double end, int N, Func<double, double, double> f)
         {
